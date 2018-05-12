73c925311394305ae7da6a38859279da7131da88
GordonBryden <gordon.bryden@gmail.com>
2018-05-12 10:23:53 +0100

    add the guts of the query builder
    Still has trouble with most datasets, which return components that aren't dimensions or values. Needs either another filter for non-dimensions, or a cleverer dataset_structure

--- R/scotgov_get.R
+++ R/scotgov_get.R
@@ -13,4 +13,44 @@
 #'
 #' @examples
 #' scotgov_get("http://statistics.gov.scot/sparql")
-scotgov_get <- function(endpoint = "http://statistics.gov.scot/sparql") {}
+
+require(SPARQL)
+
+scotgov_get <- function(dataset) {
+  
+  endpoint <- "http://statistics.gov.scot/sparql"
+  
+  components <- get_names(dataset_structure(dataset))
+  measures <- get_names(dataset_measures(dataset))
+  dimensions <- components[!components %in% measures]
+  locations <- unlist(lapply(dimensions,location_generator))
+  question_marked_dimensions <-unlist(lapply(dimensions,function(x) paste0('?',x)))
+  
+  #start the sparql query
+  select_line <- paste(paste("select",paste(question_marked_dimensions,collapse=" ")),"?value")
+  data_line <- paste0("?data qb:dataSet <",dataset,">.")
+  
+  query<- paste("PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ",
+                select_line,
+                "where {",
+                data_line)
+  
+  #iter over the dimensions, and generate a sparql line
+  for (i in length(locations)) {
+    query_addition <- paste0("?data ",
+                             locations[i],
+                             " ",
+                             question_marked_dimensions[i],
+                             ".")
+    query <- paste(query, query_addition)
+  }
+  
+  #expose the measureType dimension's value as a value
+  query<-paste(query, "?data ?measureType ?value. }")
+  
+  query_data <- SPARQL(endpoint,query)
+  result <- query_data$results
+  
+  return(result)
+  
+}
