6a3ea07e7528267efb282cc2acf876fb454da01b
GordonBryden <gordon.bryden@gmail.com>
2018-07-16 20:35:37 +0100

    Update scotgov_get.R

--- R/scotgov_get.R
+++ R/scotgov_get.R
@@ -28,7 +28,7 @@ scotgov_get <- function(dataset,start_date=NULL,end_date=NULL,geography=NULL) {
   select_line <- paste(paste("select",paste(question_marked_dimensions,collapse=" ")),"?value")
   data_line <- paste0("?data qb:dataSet <http://statistics.gov.scot/data/",dataset,">.")
 
-  query<- paste("PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ",
+  query<- paste("PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>",
                 select_line,
                 "where {",
                 data_line)
