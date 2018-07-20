d2f23a7d2a2861c6ea9c22c29f228d774df90069
GordonBryden <gordon.bryden@gmail.com>
2018-07-20 08:49:34 +0100

    reupload

--- R/scotgov_get.R
+++ R/scotgov_get.R
@@ -16,61 +16,61 @@
 #'
 #' @export
 scotgov_get <- function(dataset,start_date=NULL,end_date=NULL,geography=NULL) {
-
+  
   endpoint <- "http://statistics.gov.scot/sparql"
   TEMP_locations <- dataset_dimensions(dataset)
   locations <- data.frame(lapply(TEMP_locations, as.character), stringsAsFactors=FALSE)
   dimensions <- gsub("[()%]", "", get_names(locations[,]))
   question_marked_dimensions <-unlist(lapply(dimensions,function(x) paste0('?',x)))
   uri_dimensions <-unlist(lapply(question_marked_dimensions,function(x) paste0(x,"URI")))
-
+  
   #start the sparql query
   select_line <- paste(paste("select",paste(question_marked_dimensions,collapse=" ")),"?value")
   data_line <- paste0("?data qb:dataSet <http://statistics.gov.scot/data/",dataset,">.")
-
+  
   query<- paste("PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>",
                 select_line,
                 "where {",
                 data_line)
-
-   #iter over the dimensions, and generate a sparql line
-   for (i in 1:length(locations[,])) {
-     query_addition <- paste0("?data ",
-                              locations[i,],
-                              " ",
-                              uri_dimensions[i],
-                              ". ",
-                              uri_dimensions[i],
-                              " rdfs:label ",
-                              question_marked_dimensions[i],
-                              ".")
-
-     query <- paste(query, query_addition)
-   }
-   
-    #filter based on date if requested
-    if(!is.null(start_date) & !is.null(end_date)) {
-        query_addition <- paste0("FILTER (?refPeriod >= '",start_date,"'^^xsd:date && ?refPeriod <= '",end_date,"'^^xsd:date)")
-        query <- paste(query, query_addition)
-    } else if(!is.null(start_date)) {
-        query_addition <- paste0("FILTER (?refPeriod >= '",start_date,"'^^xsd:date)")
-        query <- paste(query, query_addition)
-    } else if(!is.null(end_date)) {
-        query_addition <- paste0("FILTER (?refPeriod <= '",end_date,"'^^xsd:date)")
-        query <- paste(query, query_addition)
-    }
-      
-   #filter based on geography if requested
-   if(!is.null(geography)) {
-       query_addition <- paste0("FILTER( regex(str(?refAreaURI), '",geography,"' ))")
-       query <- paste(query, query_addition)
-   }
-      
-   #expose the measureType dimension's value as a value
-   query<-paste(query, "?data ?measureTypeURI ?value. }")
-   query_data <- SPARQL(endpoint,query)
-   result <- query_data$results
-
-   return(result)
-
+  
+  #iter over the dimensions, and generate a sparql line
+  for (i in 1:length(locations[,])) {
+    query_addition <- paste0("?data ",
+                             locations[i,],
+                             " ",
+                             uri_dimensions[i],
+                             ". ",
+                             uri_dimensions[i],
+                             " rdfs:label ",
+                             question_marked_dimensions[i],
+                             ".")
+    
+    query <- paste(query, query_addition)
+  }
+  
+  #filter based on date if requested
+  if(!is.null(start_date) & !is.null(end_date)) {
+    query_addition <- paste0("FILTER (?refPeriod >= '",start_date,"'^^xsd:date && ?refPeriod <= '",end_date,"'^^xsd:date)")
+    query <- paste(query, query_addition)
+  } else if(!is.null(start_date)) {
+    query_addition <- paste0("FILTER (?refPeriod >= ",start_date,"^^xsd:date)")
+    query <- paste(query, query_addition)
+  } else if(!is.null(end_date)) {
+    query_addition <- paste0("FILTER (?refPeriod <= ",end_date,"^^xsd:date)")
+    query <- paste(query, query_addition)
+  }
+  
+  #filter based on geography if requested
+  if(!is.null(geography)) {
+    query_addition <- paste0("FILTER( regex(str(?refAreaURI), '",geography,"' ))")
+    query <- paste(query, query_addition)
+  }
+  
+  #expose the measureType dimension's value as a value
+  query<-paste(query, "?data ?measureTypeURI ?value. }")
+  query_data <- SPARQL(endpoint,query)
+  result <- query_data$results
+  
+  return(result)
+  
 }
