cd8312f9bfa145e9714f5356776185a6db1ba598
jsphdms <joseph@josephadams.net>
2018-10-18 10:00:13 +0100

    lint scotgov_get()

--- R/scotgov_get.R
+++ R/scotgov_get.R
@@ -15,43 +15,64 @@
 #' scotgov_get("http://statistics.gov.scot/sparql")
 #'
 #' @export
-scotgov_get <- function(dataset,start_date=NULL,end_date=NULL,geography=NULL) {
-  
-  if(is.null(start_date) &
-     is.null(end_date) & 
-     is.null(geography)) {
-    
-  if("readr" %in% rownames(installed.packages())) {
+scotgov_get <- function(dataset,
+                        start_date=NULL,
+                        end_date=NULL,
+                        geography=NULL) {
+
+  if (is.null(start_date) & is.null(end_date) & is.null(geography)) {
+
+  if ("readr" %in% rownames(installed.packages())) {
     #download with readr if available
-    result <- readr::read_csv(paste0("https://statistics.gov.scot/downloads/cube-table?uri=http%3A%2F%2Fstatistics.gov.scot%2Fdata%2F",dataset))
+    result <- readr::read_csv(paste0("https://statistics.gov.scot/downloads/",
+                                     "cube-table?uri=http%3A%2F%2F",
+                                     "statistics.gov.scot%2Fdata%2F",
+                                     dataset))
   } else {
-    result <- read.csv(paste0("https://statistics.gov.scot/downloads/cube-table?uri=http%3A%2F%2Fstatistics.gov.scot%2Fdata%2F",dataset))
+    result <- read.csv(paste0("https://statistics.gov.scot/downloads/",
+                              "cube-table?uri=http%3A%2F%2F",
+                              "statistics.gov.scot%2Fdata%2F",
+                              dataset))
   }
-    
-    result <- read.csv(paste0("https://statistics.gov.scot/downloads/cube-table?uri=http%3A%2F%2Fstatistics.gov.scot%2Fdata%2F",dataset))
-    
+
+    result <- read.csv(paste0("https://statistics.gov.scot/downloads/",
+                              "cube-table?uri=http%3A%2F%2F",
+                              "statistics.gov.scot%2Fdata%2F",
+                              dataset))
+
   } else {
-  
+
   endpoint <- "http://statistics.gov.scot/sparql"
   TEMP_locations <- dataset_dimensions(dataset)
-  locations <- data.frame(lapply(TEMP_locations, as.character), stringsAsFactors=FALSE)
-  dimensions <- gsub("[()%]", "", get_names(locations[,]))
-  question_marked_dimensions <-unlist(lapply(dimensions,function(x) paste0('?',x)))
-  uri_dimensions <-unlist(lapply(question_marked_dimensions,function(x) paste0(x,"URI")))
-  
+  locations <- data.frame(lapply(TEMP_locations, as.character),
+                          stringsAsFactors = FALSE)
+  dimensions <- gsub("[()%]", "", get_names(locations[, ]))
+
+  question_marked_dimensions <- unlist(
+    lapply(dimensions, function(x) paste0("?", x))
+    )
+
+  uri_dimensions <- unlist(
+    lapply(question_marked_dimensions, function(x) paste0(x, "URI"))
+    )
+
   #start the sparql query
-  select_line <- paste(paste("select",paste(question_marked_dimensions,collapse=" ")),"?value")
-  data_line <- paste0("?data qb:dataSet <http://statistics.gov.scot/data/",dataset,">.")
-  
-  query<- paste("PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>",
-                select_line,
-                "where {",
-                data_line)
-  
+  select_line <- paste("select", paste(question_marked_dimensions), "?value")
+  data_line <- paste0("?data qb:dataSet <http://statistics.gov.scot/data/",
+                      dataset,
+                      ">.")
+
+  query <- paste("PREFIX qb: <http://purl.org/linked-data/cube#> ",
+                 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ",
+                 "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>",
+                 select_line,
+                 "where {",
+                 data_line)
+
   #iter over the dimensions, and generate a sparql line
-  for (i in 1:length(locations[,])) {
+  for (i in 1:length(locations[, ])) {
     query_addition <- paste0("?data ",
-                             locations[i,],
+                             locations[i, ],
                              " ",
                              uri_dimensions[i],
                              ". ",
@@ -59,40 +80,55 @@ scotgov_get <- function(dataset,start_date=NULL,end_date=NULL,geography=NULL) {
                              " rdfs:label ",
                              question_marked_dimensions[i],
                              ".")
-    
+
     query <- paste(query, query_addition)
   }
-  
+
   #filter based on date if requested
-  if(!is.null(start_date) & !is.null(end_date)) {
-    query_addition <- paste0("FILTER (?refPeriod >= '",start_date,"'^^xsd:date && ?refPeriod <= '",end_date,"'^^xsd:date)")
+  if (!is.null(start_date) & !is.null(end_date)) {
+
+    query_addition <- paste0("FILTER (?refPeriod >= '",
+                             start_date,
+                             "'^^xsd:date && ?refPeriod <= '",
+                             end_date,
+                             "'^^xsd:date)")
     query <- paste(query, query_addition)
-  } else if(!is.null(start_date)) {
-    query_addition <- paste0("FILTER (?refPeriod >= '",start_date,"'^^xsd:date)")
+
+  } else if (!is.null(start_date)) {
+    query_addition <- paste0("FILTER (?refPeriod >= '",
+                             start_date,
+                             "'^^xsd:date)")
     query <- paste(query, query_addition)
-  } else if(!is.null(end_date)) {
-    query_addition <- paste0("FILTER (?refPeriod <= '",end_date,"'^^xsd:date)")
+
+  } else if (!is.null(end_date)) {
+    query_addition <- paste0("FILTER (?refPeriod <= '",
+                             end_date,
+                             "'^^xsd:date)")
     query <- paste(query, query_addition)
   }
-  
+
   #filter based on geography if requested
-  if(!is.null(geography)) {
-    query_addition <- paste0("FILTER( regex(str(?refAreaURI), '",geography,"' ))")
+  if (!is.null(geography)) {
+    query_addition <- paste0("FILTER( regex(str(?refAreaURI), '",
+                             geography,
+                             "' ))")
     query <- paste(query, query_addition)
   }
-  
+
   #expose the measureType dimension's value as a value
-  query<-paste(query, "?data ?measureTypeURI ?value. }")
-    
-  query_data <- try(SPARQL(endpoint,query),silent = TRUE)
-  if( query_data[1] ==  "Error : XML content does not seem to be XML: 'Response too large'\n"){
-    stop(Error = "Dataset is too large to be downloaded like this. Try adding filters to reduce size")
-  }    
-  
-  result <- query_data$results
-  
+  query <- paste(query, "?data ?measureTypeURI ?value. }")
+
+  query_data <- try(SPARQL::SPARQL(endpoint, query), silent = TRUE)
+  if ( query_data[1] ==
+       "Error : XML content does not seem to be XML: 'Response too large'\n"){
+    stop(Error = paste("Dataset is too large to be downloaded like this.",
+                       "Try adding filters to reduce size"))
   }
-                                 
+
+  result <- query_data$results
+
+  }
+
   return(result)
-  
+
 }
