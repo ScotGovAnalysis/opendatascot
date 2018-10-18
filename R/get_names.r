9da31b74078d41dbe49c7e79b8cd94015bbaaafe
jsphdms <joseph@josephadams.net>
2018-10-18 09:33:27 +0100

    lint get_names()

--- R/get_names.r
+++ R/get_names.r
@@ -2,11 +2,17 @@
 # can be removed when dataset_structure and dataset_measures return clear labels
 # internal function for scotgov_get
 
-get_names<- function(dataset) {
-  pattern<-'[/#][%A-Za-z0-9()-]+>'
-  regexpr_result<-regexpr(pattern,dataset)
-  result<-substr(dataset,regexpr_result+1,regexpr_result+attr(regexpr_result,"match.length")-2)
-  
+get_names <- function(dataset) {
+
+  pattern <- "[/#][%A-Za-z0-9()-]+>"
+
+  regexpr_result <- regexpr(pattern, dataset)
+
+  result <- substr(dataset,
+                   regexpr_result + 1,
+                   regexpr_result + attr(regexpr_result,
+                                         "match.length") - 2)
+
   return(result)
-  
-}  
+
+}
