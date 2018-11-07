eba6880278740c4c723b3fb2a25e6573b51ba166
GordonBryden <gordon.bryden@gmail.com>
2018-11-07 11:07:14 +0000

    fix errors

--- R/get_structure.r
+++ R/get_structure.r
@@ -1,16 +1,20 @@
 get_structure <- function(dataset) {
-  
+
   dimension_set <- dataset_dimensions(dataset)
-  
+  names(dimension_set) <- "dimensions"
+
   categories <- list()
-  
-  for(i in 1:length(dimensions_set)) {
-    
-    categories[i] <- get_categories(dataset, dimension_set[i])
-    
+
+  for(i in 1:length(dimension_set[,])) {
+
+    categories[[i]] <- get_categories(dataset, dimension_set[i,])
+
+
   }
-  
+  names(categories) <- get_names(dimension_set[,])
+
   result <- list(dimension_set,categories)
-  
+  names(result) <- list("dimensions","categories")
+
   return(result)
 }
