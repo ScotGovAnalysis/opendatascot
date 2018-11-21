a89888ff6edf4ce04444379709d02022bf8a9484
GordonBryden <gordon.bryden@gmail.com>
2018-11-21 11:56:53 +0000

    Add SPARQL:: to SPARQL call

--- R/scotgov_get.R
+++ R/scotgov_get.R
@@ -49,7 +49,7 @@ scotgov_get <- function(dataset,
     
   query <- get_dataset_query(dataset)
     
-  query_data <- try(SPARQL(endpoint,query),silent = TRUE)
+  query_data <- try(SPARQL::SPARQL(endpoint,query),silent = TRUE)
   if( query_data[1] ==  "Error : XML content does not seem to be XML: 'Response too large'\n"){
     stop(Error = "Dataset is too large to be downloaded like this. Try adding filters to reduce size")
   }    
