de9a36d68abb508e732a267915a4b1ad0a1f6cbf
Thomas Crines <thomas.crines@hotmail.com>
2019-01-10 11:27:07 +0000

    refactored from lintr, tests passing

--- tests/testthat/test_ods_error_message.R
+++ tests/testthat/test_ods_error_message.R
@@ -2,34 +2,50 @@ context("ods_error_message")
 
 test_that("non-existent dataset produces correct message", {
   skip_on_cran()
-  expect_message(scotgov_get("made-up-dataset"), "The dataset 'made-up-dataset' does not exist.\nCompare with existing datasets using 'ods_all_datasets'.")
+  expect_message(scotgov_get("made-up-dataset"),
+                 "The dataset 'made-up-dataset' does not exist.",
+                 "Compare with existing datasets using 'ods_all_datasets'.")
 })
 
-# Test not working due to return characters, though results match in content
-# test_that("non-existent dataset produces correct message with filter", {
-#   skip_on_cran()
-#   expect_message(scotgov_get("made-up-dataset", 2017), "There was an unknown error in the generated query; possibly an error in the dataset name provided.\nThe dataset name provided was: 'made-up-dataset'.\nCompare with existing datasets using 'ods_all_datasets()'.")
-# })
+test_that("non-existent dataset produces correct message with filter", {
+  skip_on_cran()
+  expect_message(scotgov_get("made-up-dataset", 2017),
+                 "There was an unknown error in the generated query; possibly an error in the dataset name provided.",
+                 "The dataset name provided was: 'made-up-dataset'.",
+                 "Compare with existing datasets using 'ods_all_datasets()'.")
+})
 
-# Test passing but runs over a long time
+# Test passing but takes a long time to run
 # test_that("large dataset produces correct message", {
 #   skip_on_cran()
-#   expect_message(scotgov_get("working-age-claimants-of-benefits-key"), message("The dataset 'made-up-dataset' does not exist.\nCompare with existing datasets using 'ods_all_datasets'."))
+#   expect_message(scotgov_get("working-age-claimants-of-benefits-key"),
+#                  "There was an unknown error in the generated query; possibly an error in the dataset name provided.",
+#                  "The dataset name provided was: 'working-age-claimants-of-benefits-key' does not exist.",
+#                  "Compare with existing datasets using 'ods_all_datasets()'.",
+#                  "Original error message: Error in open.connection(con, 'rb'): HTTP error 500.")
 # })
 
 test_that("large dataset produces correct message with filter", {
   skip_on_cran()
-  expect_message(scotgov_get("working-age-claimants-of-benefits-key", 2017),  "Dataset 'working-age-claimants-of-benefits-key' is too large to be downloaded in its entirety.\nTry adding filters to reduce size.")
+  expect_message(scotgov_get("working-age-claimants-of-benefits-key", 2017),
+                 "Dataset 'working-age-claimants-of-benefits-key' is too large to be downloaded in its entirety.",
+                 "Try adding filters to reduce size.")
 })
 
-# Test not working due to return characters, though results match in content
-# test_that("syntax error produces correct message", {
-#   skip_on_cran()
-#   expect_message(scotgov_get("spaces in dataset"), "There was an unknown error in the generated query; possibly an error in the dataset name provided.\nThe dataset name provided was: 'spaces in dataset'.\nCompare with existing datasets using 'ods_all_datasets()'.")
-# })
+test_that("syntax error produces correct message", {
+  skip_on_cran()
+  expect_message(scotgov_get("spaces in dataset"),
+                 "There was an unknown error in the generated query; possibly an error in the dataset name provided.",
+                 "The dataset name provided was: 'spaces in dataset'.",
+                 "Compare with existing datasets using 'ods_all_datasets()'.",
+                 "Original error message: Error in open.connection(con, 'rb'): HTTP error 400.")
+})
 
-# Test not working due to return characters, though results match in content
-# test_that("syntax error produces correct message with filter", {
-#   skip_on_cran()
-#   expect_message(scotgov_get("spaces in dataset", 2017), "There was an unknown error in the generated query; possibly an error in the dataset name provided.\nThe dataset name provided was: 'spaces in dataset'.\nCompare with existing datasets using 'ods_all_datasets()'")
-# })
+test_that("syntax error produces correct message with filter", {
+  skip_on_cran()
+  expect_message(scotgov_get("spaces in dataset", 2017),
+                 "There was an unknown error in the generated query; possibly an error in the dataset name provided.",
+                 "The dataset name provided was: 'spaces in dataset'.",
+                 "Compare with existing datasets using 'ods_all_datasets()'",
+                 "Original error message: Error in if (query != '') {: argument is of length zero")
+})
