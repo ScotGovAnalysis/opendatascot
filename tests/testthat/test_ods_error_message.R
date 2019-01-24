baa3ea4149cb1f21fa85fc7bfeda099eb4de3e02
Thomas Crines <thomas.crines@hotmail.com>
2019-01-24 10:09:39 +0000

    added error tests

---
+++ tests/testthat/test_ods_error_message.R
@@ -0,0 +1,51 @@
+context("ods_error_message")
+
+test_that("non-existent dataset produces correct message", {
+  skip_on_cran()
+  expect_message(scotgov_get("made-up-dataset"),
+                 "The dataset 'made-up-dataset' does not exist.",
+                 "Compare with existing datasets using 'ods_all_datasets'.")
+})
+
+test_that("non-existent dataset produces correct message with filter", {
+  skip_on_cran()
+  expect_message(scotgov_get("made-up-dataset", 2017),
+                 "There was an unknown error in the generated query; possibly an error in the dataset name provided.",
+                 "The dataset name provided was: 'made-up-dataset'.",
+                 "Compare with existing datasets using 'ods_all_datasets()'.")
+})
+
+# Test passing but takes a long time to run
+test_that("large dataset produces correct message", {
+  skip_on_cran()
+  expect_message(scotgov_get("working-age-claimants-of-benefits-key"),
+                 "There was an unknown error in the generated query; possibly an error in the dataset name provided.",
+                 "The dataset name provided was: 'working-age-claimants-of-benefits-key' does not exist.",
+                 "Compare with existing datasets using 'ods_all_datasets()'.",
+                 "Original error message: Error in open.connection(con, 'rb'): HTTP error 500.")
+})
+
+test_that("large dataset produces correct message with filter", {
+  skip_on_cran()
+  expect_message(scotgov_get("working-age-claimants-of-benefits-key", 2017),
+                 "Dataset 'working-age-claimants-of-benefits-key' is too large to be downloaded in its entirety.",
+                 "Try adding filters to reduce size.")
+})
+
+test_that("syntax error produces correct message", {
+  skip_on_cran()
+  expect_message(scotgov_get("spaces in dataset"),
+                 "There was an unknown error in the generated query; possibly an error in the dataset name provided.",
+                 "The dataset name provided was: 'spaces in dataset'.",
+                 "Compare with existing datasets using 'ods_all_datasets()'.",
+                 "Original error message: Error in open.connection(con, 'rb'): HTTP error 400.")
+})
+
+test_that("syntax error produces correct message with filter", {
+  skip_on_cran()
+  expect_message(scotgov_get("spaces in dataset", 2017),
+                 "There was an unknown error in the generated query; possibly an error in the dataset name provided.",
+                 "The dataset name provided was: 'spaces in dataset'.",
+                 "Compare with existing datasets using 'ods_all_datasets()'",
+                 "Original error message: Error in if (query != '') {: argument is of length zero")
+})
