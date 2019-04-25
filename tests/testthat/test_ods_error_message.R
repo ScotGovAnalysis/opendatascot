44abde673c047faa0ab789244b58d45a061e2521
Thomas Crines <thomas.crines@hotmail.com>
2019-04-25 14:45:50 +0100

    changed wording of error message large response names

--- tests/testthat/test_ods_error_message.R
+++ tests/testthat/test_ods_error_message.R
@@ -14,14 +14,14 @@ test_that("non-existent dataset produces correct message with filter", {
                              , fixed = TRUE))
 })
 
-test_that("large dataset produces correct message", {
+test_that("response too large produces correct message", {
   skip_on_cran()
   expect_that(ods_dataset("working-age-claimants-of-benefits-key")
               , throws_error("The dataset 'working-age-claimants-of-benefits-key' is too large to be downloaded directly.\nTry adding filters to reduce size or contact statistics.enquiries@gov.scot if you require the full dataset."
                              , fixed = TRUE))
 })
 
-test_that("large dataset produces correct message with filter", {
+test_that("response too large produces correct message with filter", {
   skip_on_cran()
   expect_that(ods_dataset("working-age-claimants-of-benefits-key", start_date = 2017)
               , throws_error("The dataset 'working-age-claimants-of-benefits-key' is too large to be downloaded directly.\nTry adding filters to reduce size or contact statistics.enquiries@gov.scot if you require the full dataset."
