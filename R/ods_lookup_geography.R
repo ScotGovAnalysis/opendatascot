bdcaf39960249be616968e47c798fb5b3b34e8eb
GordonBryden <gordon.bryden@gmail.com>
2019-07-03 10:14:42 +0100

    rename function

--- R/ods_lookup_geography.R
+++ R/ods_lookup_geography.R
@@ -1,4 +1,4 @@
-geography_lookup <- function(geography) {
+ods_lookup_geography <- function(geography) {
 geo_lookup <- read.csv("http://statistics.gov.scot/downloads/file?id=f08c946e-cdfa-44de-9855-2b278a2aaf93%2FDatazone2011Lookup.csv", stringsAsFactors = FALSE)
 geo_lookup <- geo_lookup[c(1,2,6,7,10)]
 colnames(geo_lookup) <- c("datazone", "interzone", "local_authority", "health_board", "country")
