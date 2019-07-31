58a23ebb320648287a58f6d3c8f0df2afb5c1ffa
GordonBryden <gordon.bryden@gmail.com>
2019-07-31 10:49:01 +0100

    Create test for arbitrary filtering

--- tests/testthat/test_ods_dataset.R
+++ tests/testthat/test_ods_dataset.R
@@ -39,3 +39,9 @@ test_that("ods_dataset with geography filtering returns correct ref area", {
   skip_on_cran()
   expect_equal(ods_dataset(dataset = "average-household-size", geography = "S12000039")$refArea[1], "West Dunbartonshire")
 })
+
+test_that("ods_dataset with arbitrary filtering returns correct value", {
+  skip_on_cran()
+  expect_equal(ods_dataset(dataset = "average-household-size", refArea="West Lothian", refPeriod="2018", measureType="Ratio")$value
+, 2.31)
+})
