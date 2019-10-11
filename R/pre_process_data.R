6ac446ff6093d5705f070f0cc55ce51f1b2da80e
GordonBryden <gordon.bryden@gmail.com>
2019-10-11 12:58:13 +0100

    Update pre_process_data.R

--- R/pre_process_data.R
+++ R/pre_process_data.R
@@ -1,3 +1,18 @@
+#' Strips all api cruft form returned values
+#'
+#' \code{pre_process_data} is an internal function for \code{ods_dataset} that returns all unique values of a dataset scheme
+#'
+#' @param x \code{data.frame}. Dataframe produced by the sparql query.
+#'
+#' @return \code{data.frame}.
+#'
+#' @examples
+#' pre_process_data(my_query_results)
+#'
+#' @keywords internal
+#'
+#' @noRd
+
 pre_process_data <- function(x) {
   # Keep only last elemnt of URI
   x[] <- lapply(
