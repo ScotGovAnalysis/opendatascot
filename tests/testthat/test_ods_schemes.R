200021bba21a23f99dc3af8a34e51149553bdf66
GordonBryden <gordon.bryden@gmail.com>
2020-03-02 15:43:18 +0000

    New tests

---
+++ tests/testthat/test_ods_schemes.R
@@ -0,0 +1,15 @@
+context("ods_scheme")
+
+test_that("Entered a dataset with a space",
+    expect_that(ods_schemes("bus accessibility"),
+                throws_error("Blanks space detected in requested dataset name, replace with a dash '-' for a valid dataset name"
+                             , fixed = TRUE))
+    )
+
+with_mock_api(
+test_that("Mispelled dataset entered",
+          expect_that(ods_schemes("busaccessibility"),
+                      throws_error("No schemes detected for this dataset. Check dataset is spelled correctly"
+                                   , fixed = TRUE))
+)
+)
