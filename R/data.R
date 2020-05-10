aca1b4b909897561691e086f58b8f9357d917f56
Konrad Zdeb <konrad.zdeb@me.com>
2020-05-10 21:46:56 +0100

    Reference data
    Expeanding reference data.

--- R/data.R
+++ R/data.R
@@ -4,13 +4,24 @@
 #'   geographies across Scotland. The included geographies account for 2001 and
 #'   2011 data zones.
 #'
-#' @details Some detail
+#' @details Data zone geographies are peridiocally revieviwed in order to
+#'   accommodate for population flows. Usually, this is done following
+#'   availability of new census data. In order to minimise size of the data set
+#'   only the closest match of with the 2001 data zone was kept.
 #'
 #' @format A data frame with 53940 rows and 10 variables:
 #' \itemize{
-#'   \item{\code{datazone_2001}}{ - 2001 data zone code}
-#'   \item{\code{datazone_2011}}{ - 2011 data zone code}
+#'   \item{Datazones:}
+#'      \itemize{
+#'        \item{\code{datazone_2001}}{ - 2001 data zone code}
+#'        \item{\code{datazone_2011}}{ - 2011 data zone code}
+#'   }
+#'   \item{Higher geographies:}
+#'      \itemize{
+#'        \item{\code{InterZone}}{ - Intermediate Geography Zone 2011}
+#'      }
 #' }
 #' @source
 #' \emph{Scottish Government,} 2015. \href{https://www2.gov.scot/Topics/Statistics/sns/SNSRef/DZMatchingFile}{Data Zone Matching File (Area and Pop. based 2001 - 2011)}
+#' \emph{Scottish Government,} 2014. \href{https://www2.gov.scot/Topics/Statistics/sns/SNSRef/DZ2011Lookups}{Data Zone and Intermediate Zone 2011 Lookups}
 "geo_lookup"
--- data-raw/geo_lookup.R
+++ data-raw/geo_lookup.R
@@ -2,36 +2,74 @@
 # The following script prepares geographic lookup data. The data provides simple
 # lookup tables linking data zone to common high level geographies.
 
-# Packages
-library("tidyverse")
-library("readxl")
+# Packages ----------------------------------------------------------------
+
+library("tidyverse")  # Easier data manipulation
+library("readxl")     # Importing MS Excel files
+
+# Downloads ---------------------------------------------------------------
+
+# Relevant sources are downloaded and stored
+
+sources <-
+  map(
+    .x = c(
+      "https://www2.gov.scot/Resource/0048/00483037.xlsx",
+      "https://www2.gov.scot/Resource/0046/00462937.csv"
+    ),
+    .f = function(.x) {
+      t <- tempfile()
+      download.file(.x, t)
+      t
+    }
+  )
+
+# Imports -----------------------------------------------------------------
+
+# Read imported files
 
 # Source 2001 and 2011 data zones
 dta_datazones <- read_xlsx(
-  path = {
-    tmp_xlsx_dz_lookup <-
-      tempfile(pattern = "dz_lookup", fileext = ".xlsx")
-    download.file(url = "https://www2.gov.scot/Resource/0048/00483037.xlsx",
-                  destfile = tmp_xlsx_dz_lookup)
-    tmp_xlsx_dz_lookup
-  },
+  path = sources[[1]],
   sheet = "AreaMatch",
   col_names = c(
-    "datazone_2001",
     "datazone_2011",
+    "datazone_2001",
     "area_match",
     "pct_2011_area"
   ),
   col_types = c(
     datazone_2011 = "text",
-    datazone_2011 = "text",
+    datazone_2001 = "text",
     area_match = "numeric",
     pct_2011_area = "numeric"
   ),
   range = "A2:D25075"
+) %>%
+  arrange(desc(pct_2011_area)) %>%
+  distinct(datazone_2011, .keep_all = TRUE) %>%
+  arrange(datazone_2011) %>%
+  select(datazone_2011, datazone_2001)
+
+# Prepare higher geography lookup for 2011 data zones
+dta_higher_geos <- read_csv(
+  file = sources[[2]],
+  col_names = c(
+    "DataZone",
+    "InterZone",
+    "MMWard",
+    "Council",
+    "SPConst",
+    "CHP",
+    "HBCode"
+  ),
+  col_types = cols(.default = col_character()),
+  skip = 1
 )
 
-geo_lookup <- mtcars
+
+
+# geo_lookup <- dta_datazones
 
 # Embed geo_lookup data within package
-usethis::use_data(geo_lookup, overwrite = TRUE)
+# usethis::use_data(geo_lookup, overwrite = TRUE)
