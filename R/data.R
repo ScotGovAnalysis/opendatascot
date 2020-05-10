8ad0de70b0a24766e70d22bcc8899959096eead8
Konrad Zdeb <konrad.zdeb@me.com>
2020-05-10 16:08:43 +0100

    Reference data development
    Major additions:
     - Documentation stub to be populated with more details
     - Initial import file reading data zone lookup file provided by the
       Scottish Government, to be expanded with more imports
     - Minor configuration changes
    
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- R/data.R
+++ R/data.R
@@ -1,13 +1,16 @@
 #' @title Geographic Lookup Data
 #'
 #' @description A simple lookup data set linking data zones to high-level
-#'   geographies across Scotland.
+#'   geographies across Scotland. The included geographies account for 2001 and
+#'   2011 data zones.
+#'
+#' @details Some detail
 #'
 #' @format A data frame with 53940 rows and 10 variables:
-#' \describe{
-#'   \item{colA}{description}
-#'   \item{colB}{description}
-#'   ...
+#' \itemize{
+#'   \item{\code{datazone_2001}}{ - 2001 data zone code}
+#'   \item{\code{datazone_2011}}{ - 2011 data zone code}
 #' }
-#' @source \url{http://www.where-i-live.com}
+#' @source
+#' \emph{Scottish Government,} 2015. \href{https://www2.gov.scot/Topics/Statistics/sns/SNSRef/DZMatchingFile}{Data Zone Matching File (Area and Pop. based 2001 - 2011)}
 "geo_lookup"
--- data-raw/geo_lookup.R
+++ data-raw/geo_lookup.R
@@ -2,7 +2,36 @@
 # The following script prepares geographic lookup data. The data provides simple
 # lookup tables linking data zone to common high level geographies.
 
-# Source and prepare the data
+# Packages
+library("tidyverse")
+library("readxl")
+
+# Source 2001 and 2011 data zones
+dta_datazones <- read_xlsx(
+  path = {
+    tmp_xlsx_dz_lookup <-
+      tempfile(pattern = "dz_lookup", fileext = ".xlsx")
+    download.file(url = "https://www2.gov.scot/Resource/0048/00483037.xlsx",
+                  destfile = tmp_xlsx_dz_lookup)
+    tmp_xlsx_dz_lookup
+  },
+  sheet = "AreaMatch",
+  col_names = c(
+    "datazone_2001",
+    "datazone_2011",
+    "area_match",
+    "pct_2011_area"
+  ),
+  col_types = c(
+    datazone_2011 = "text",
+    datazone_2011 = "text",
+    area_match = "numeric",
+    pct_2011_area = "numeric"
+  ),
+  range = "A2:D25075"
+)
+
+geo_lookup <- mtcars
 
 # Embed geo_lookup data within package
 usethis::use_data(geo_lookup, overwrite = TRUE)
